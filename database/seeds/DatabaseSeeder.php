7f407f85a9236760db32fff605701fe62917e279
Unknown <napat.sa@ku.th>
2017-05-04 23:48:29 +0700

    add tables and add db:seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Car;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,29 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $obj1 = new Car();
+        $obj1->register = 'กท1111';
+        $obj1->type = 'SUV';
+        $obj1->brand = 'HONDA';
+        $obj1->seat = '5';
+        $obj1->pricePerDay = '1500';
+        $obj1->save();
+
+        $obj2 = new Car();
+        $obj2->register = 'กท2222';
+        $obj2->type = 'VAN';
+        $obj2->brand = 'HONDA';
+        $obj2->seat = '9';
+        $obj2->pricePerDay = '2000';
+        $obj2->save();
+
+        $obj3 = new Car();
+        $obj3->register = 'กท3333';
+        $obj3->type = 'Medium Car';
+        $obj3->brand = 'HONDA';
+        $obj3->seat = '4';
+        $obj3->pricePerDay = '800';
+        $obj3->save();
+
     }
 }

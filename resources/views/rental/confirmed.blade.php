638259f52c31265ea8da1bc5fe92e5e9bf14a828
Unknown <napat.sa@ku.th>
2017-05-05 00:30:28 +0700

    add confirmed.blade.php in rental

---
+++ resources/views/rental/confirmed.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.master')
+
+@section('content')
+<h1 class="title">Your booking was successfully !</h1><br>
+<table class="table table-bordered ">
+  <thead>
+    <tr>
+      <th colspan="2" class="bg-info">Driver Information</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th width="15%" scope="row">Firstname</th>
+      <td>{{$firstName}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Lastname</th>
+      <td>{{$lastName}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Email</th>
+      <td>{{$email}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Phone Number</th>
+      <td>{{$phoneNum}}</td>
+    </tr>
+  </tbody>
+</table>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th colspan="2" class="bg-info">Car Information</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th width="15%" scope="row">Register</th>
+      <td>{{$car->register}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Type</th>
+      <td>{{$car->type}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Brand</th>
+      <td>{{$car->brand}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Seat</th>
+      <td>{{$car->seat}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Pick up date</th>
+      <td>{{$pickDate}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Drop off date</th>
+      <td>{{$dropDate}}</td>
+    </tr>
+    <tr>
+      <th scope="row">Total Price</th>
+      <td>{{$dateDiff*$car->pricePerDay}} Baht</td>
+    </tr>
+  </tbody>
+</table>
+<button class="btn btn-primary center-block" type="button" name="button" onclick="window.print()">PRINT</button>
+
+@endsection
